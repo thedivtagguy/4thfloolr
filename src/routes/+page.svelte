fa0803721c2efeaae25b20602b2df2f98ddb5059
thedivtagguy <amanbhargava2001@gmail.com>
2023-09-06 22:10:48 +0530

    Add images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/t1.webp';
-	import welcome_fallback from '$lib/images/t1.png';
+	import hall from '$lib/images/hall.jpg';
+	import julie from '$lib/images/julie.jpg';
+	import machine from '$lib/images/machine.jpg';
+	import terrace from '$lib/images/terrace.jpg';
+	import kaka from '$lib/images/kaka.jpg';
 </script>
 
 <svelte:head>
@@ -12,20 +15,98 @@
 <section>
 	<h1>
 		<span class="welcome-text">The password is</span><br />sundarbois
-		
-
-		
 	</h1>
+
+	<div class="angry-grid">
+		<div id="item-0">
+			<picture>
+				<img class="landscape" src={julie} alt="Julie, our dog" />
+				<caption class="caption"
+					>Our best friend</caption
+				>
+			</picture>
+		</div>
+		<div id="item-1">
+			<picture>
+				<img class="portrait" src={machine} alt="Julie, our dog" />
+				<caption class="caption"
+					>Can I wash my clothes today?</caption
+				>
+			</picture>
+		</div>
+		<div id="item-2">
+			<picture>
+				<img class="portrait" src={hall} alt="Julie, our dog" />
+				<caption class="caption"
+					>The sovereign republic</caption
+				>
+			</picture>
+		</div>
+		<div id="item-3">
+			<picture>
+				<img class="portrait" src={kaka} alt="Julie, our dog" />
+				<caption class="caption"
+					>
+					A superhero, if there was one
+					</caption
+				>
+			</picture>
+		</div>
+		<div id="item-4">
+			<picture>
+				<img class="landscape" src={terrace} alt="Julie, our dog" />
+				<caption class="caption"
+					>Home</caption
+				>
+			</picture>
+		</div>
+	</div>
 
 	<h2>
 		<a href="https://t14f.com">t14f</a>
 	</h2>
-
-
-	
 </section>
 
 <style>
+	.angry-grid {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-content: center;
+		gap: 10px;
+	}
+
+	#item-0 {
+		flex: 1 0 100%;
+	}
+
+	#item-1,
+	#item-2,
+	#item-3 {
+		flex: 0 0 calc(33.33% - 10px); /* Adjust the width as needed with proper spacing */
+		margin-top: 50rem;
+	}
+
+	#item-4 {
+		flex: 1 0 100%;
+	}
+
+	#item-1 {
+		margin-top: 90rem;
+	}
+
+	#item-3 {
+		margin-top: 120rem;
+	}
+	.portrait {
+		max-width: 100%;
+	}
+
+	.landscape {
+		max-width: 100%;
+		margin-top: 50rem;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
@@ -38,7 +119,7 @@
 		width: 100%;
 		font-size: 5rem;
 		line-height: 1.1;
-		margin: 2rem 0;
+		margin-top: 20rem;
 	}
 
 	h2 {
@@ -51,30 +132,6 @@
 	span.welcome-text {
 		font-weight: 300;
 		font-style: italic;
-	}
-
-	span.italics {
-		font-style: italic;
-		font-weight: 300;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 50%;
-		height: 100%;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-		margin: auto;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		padding: 0.8rem 0;
-		display: block;
-		filter: drop-shadow(5px 15px 10px #222222c1)
 	}
 
 	/* Media query for screens smaller than 768px (typical mobile screens) */
@@ -90,8 +147,8 @@
 			text-align: center;
 		}
 
-		.welcome {
-			width: 50%; /* Adjust the image width for smaller screens */
+		.angry-grid {
+			flex-direction: column;
 		}
 	}
 </style>
